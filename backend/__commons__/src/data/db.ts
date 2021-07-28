45fa3e4c368ebc291b684dcdc5f2700983da6b65
maryhanakuhn <maryhanakuhn@gmail.com>
2021-07-28 19:43:10 -0300

    contacts-service rodando

---
+++ backend/__commons__/src/data/db.ts
@@ -0,0 +1,15 @@
+import { Sequelize } from "sequelize";
+
+const dbName = process.env.DB_NAME!;
+const dbUser = process.env.DB_USER!;
+const dbPassword = process.env.DB_PASSWORD;
+const dbHost = process.env.DB_HOST;
+const logging = process.env.SQL_LOG ? true : false;
+
+const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
+  dialect: "mysql",
+  host: dbHost,
+  logging: logging,
+});
+
+export default sequelize;

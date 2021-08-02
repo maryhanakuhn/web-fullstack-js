55ab0404f633600589e150fd2c4b51a0d7673350
maryhanakuhn <maryhanakuhn@gmail.com>
2021-08-02 17:38:37 -0300

    rotas contact-service

--- backend/accounts-service/src/routes/middlewares.ts
+++ backend/accounts-service/src/routes/middlewares.ts
@@ -5,51 +5,27 @@ import {
   loginSchema,
   accountUpdateSchema,
 } from "../models/accountSchemas";
-import auth from "../auth";
-
-function validateSchema(
-  schema: Joi.ObjectSchema<any>,
-  req: Request,
-  res: Response,
-  next: any
-) {
-  const { error } = schema.validate(req.body);
-  if (error == null) return next();
-
-  const { details } = error;
-  const message = details.map((item) => item.message).join(".");
-
- // console.log(message);
-  res.status(422).end();
-}
+import commonsMiddleware from "ms-commons/api/routes/middlewares";
 
 function validateAccountSchema(req: Request, res: Response, next: any) {
-  return validateSchema(accountSchema, req, res, next);
+  return commonsMiddleware.validateSchema(accountSchema, req, res, next);
 }
 
 function validateUpdateAccountSchema(req: Request, res: Response, next: any) {
-  return validateSchema(accountUpdateSchema, req, res, next);
+  return commonsMiddleware.validateSchema(accountUpdateSchema, req, res, next);
 }
 
 function validateLoginSchema(req: Request, res: Response, next: any) {
-  return validateSchema(loginSchema, req, res, next);
+  return commonsMiddleware.validateSchema(loginSchema, req, res, next);
 }
 
 async function validateAuth(req: Request, res: Response, next: any) {
-  try {
-    const token = req.headers["x-access-token"] as string;
-    if (!token) return res.status(401).end();
-
-    const payload = await auth.verify(token);
-    if (!payload) return res.status(401).end();
-
-    res.locals.payload = payload;
-
-    next();
-  } catch (error) {
-    console.log(`validadeAuth: ${error}`);
-    res.status(400).end();
-  }
+  return commonsMiddleware.validateAuth(req, res, next);
 }
 
-export { validateAccountSchema, validateLoginSchema, validateUpdateAccountSchema, validateAuth };
+export {
+  validateAccountSchema,
+  validateLoginSchema,
+  validateUpdateAccountSchema,
+  validateAuth,
+};
--- backend/contacts-service/src/controllers/contacts.ts
+++ backend/contacts-service/src/controllers/contacts.ts
@@ -1,8 +1,12 @@
 import { Request, Response } from "express";
-import repository from "../models/contactRepository"
+import repository from "../models/contactRepository";
+import controllerCommons from "ms-commons/api/controllers/controller";
+import { Token } from "ms-commons/api/auth";
 
 async function getContacts(req: Request, res: Response, next: any) {
-  repository.findAll()
+  const token = controllerCommons.getToken(res) as Token;
+  const contacts = await repository.findAll(token.accountId);
+  res.json(contacts);
 }
 
-export default {getContacts}
+export default { getContacts };

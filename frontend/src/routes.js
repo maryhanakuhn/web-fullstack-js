151fad223a0ffcb3f7806d133675f030e42b2b9d
maryhanakuhn <maryhanakuhn@gmail.com>
2021-08-20 19:04:54 -0300

    routes

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,36 +1,131 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams,
+  useRouteMatch,
+} from "react-router-dom";
 
 function Home() {
   return (
     <div>
+      <Menu />
       <h2>Dashboard</h2>
     </div>
   );
 }
 
-function Contatos() {
-    return (
-      <div>
-        <h2>Lista contatos</h2>
-      </div>
-    );
-  }
+function Contacts() {
+  //hooks
+  let { path, url } = useRouteMatch();
+  return (
+    <div>
+      <Menu />
+      <h2>Lista contatos</h2>
+      <ul>
+        <li>
+          <Link to={`${url}/1`}>Contato A</Link>
+        </li>
+        <li>
+          <Link to={`${url}/2`}>Contato B</Link>
+        </li>
+        <li>
+          <Link to={`${url}/3`}>Contato C</Link>
+        </li>
+      </ul>
 
-  function Signin() {
-    return (
-      <div>
-        <h2>Login</h2>
-      </div>
-    );
-  }
-  function Signup() {
-    return (
-      <div>
-        <h2>Cadastro</h2>
-      </div>
-    );
-  }
+      <Switch>
+        <Route exact path={path} />
+        <Route path={`${path}/:contactId`}>
+          <Contact />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+function Contact() {
+  let { contactId } = useParams();
+  return (
+    <div>
+      <h3>Contato: {contactId}</h3>
+    </div>
+  );
+}
+
+function Signin() {
+  return (
+    <div>
+      <Menu />
+      <h2>Login</h2>
+    </div>
+  );
+}
+
+function Signup() {
+  return (
+    <div>
+      <Menu />
+      <h2>Cadastro</h2>
+    </div>
+  );
+}
+
+function Messages() {
+  let { path, url } = useRouteMatch();
+  return (
+    <div>
+      <Menu />
+      <h2>Lista de mensagens</h2>
+      <ul>
+        <li>
+          <Link to={`${url}/1`}>Mensagem A</Link>
+        </li>
+        <li>
+          <Link to={`${url}/2`}>Mensagem B</Link>
+        </li>
+        <li>
+          <Link to={`${url}/3`}>Mensagem C</Link>
+        </li>
+      </ul>
+
+      <Switch>
+        <Route exact path={path} />
+        <Route path={`${path}/:messageId`}>
+          <Message />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+function Message() {
+  let { messageId } = useParams();
+  return (
+    <div>
+      <h3>Mensagem: {messageId}</h3>
+    </div>
+  );
+}
+
+function Menu() {
+  return (
+    <ul>
+      <li>
+        <Link to="/contacts">Contatos</Link>
+      </li>
+      <li>
+        <Link to="/messages">Mensagens</Link>
+      </li>
+      <li>
+        <Link to="/signin">Sair</Link>
+      </li>
+    </ul>
+  );
+}
+
 
 export default function Routes() {
   return (
@@ -42,7 +137,10 @@ export default function Routes() {
               <Home />
             </Route>
             <Route exact path="/contacts">
-              <Contatos />
+              <Contacts />
+            </Route>
+            <Route exact path="/messages">
+              <Messages />
             </Route>
             <Route exact path="/signin">
               <Signin />

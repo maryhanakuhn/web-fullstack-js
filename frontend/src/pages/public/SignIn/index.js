a3d75bbc6d2e684a4c0c1d6b4ebb780d7acba7e2
maryhanakuhn <maryhanakuhn@gmail.com>
2021-08-20 21:40:53 -0300

    signin

--- frontend/src/pages/public/SignIn/index.js
+++ frontend/src/pages/public/SignIn/index.js
@@ -1,21 +1,37 @@
 import React from "react";
 import { Button, Form, Container, Row, Col } from "react-bootstrap";
-import Logo from "../../../assets/logo.png"
+import Logo from "../../../assets/logo.png";
 
 class SignIn extends React.Component {
+  handleSignIn = async (event) => {
+    event.preventDefault();
+  };
+
   render() {
     return (
-      <Container>
-        <Row>
-          <Col>
-          <div>
-            <img src={Logo} alt="MailShrimp"/>
-          </div>
-          <h2>Login</h2>
-          <p>Informe seus dados para autenticar:</p>
+      <Container fluid>
+        <Row className="justify-content-md-center">
+          <Col xs={12} md={5}>
+            <div>
+              <img src={Logo} alt="MailShrimp" />
+            </div>
+            <h2>Login</h2>
+            <p>Informe seus dados para autenticar:</p>
+            <Form onSubmit={this.handleSignIn}>
+              <Form.Group controlId="emailGroup">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+              </Form.Group>
+              <Form.Group controlId="passwordGroup">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+              <Button block variant="secondary"  typo="submit">
+                Submit
+              </Button>
+            </Form>
           </Col>
         </Row>
-       
       </Container>
     );
   }
